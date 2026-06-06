cf60bed85acb6df7b35f5a53ea0c9fcebd389f31
DevNoteKeeper <seoyoon.developer@gmail.com>
2026-06-07 01:24:32 +0200

    add qrs and change buttons

--- Assets/Script/PopupController.cs
+++ Assets/Script/PopupController.cs
@@ -1,10 +1,32 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PopupController : MonoBehaviour
 {
     public GameObject popup;
+    [SerializeField] Canvas canvas;
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        if (canvas == null)
+        {
+            canvas = GetComponentInParent<Canvas>();
+        }
+    }
     public void CloseButton()
     {
         popup.SetActive(false);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        if (canvas == null)
+        {
+            return;
+        }
+        rectTransform.anchoredPosition
+             += eventData.delta / canvas.scaleFactor;
     }
 }
